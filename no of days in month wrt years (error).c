171731b25f10b639a35ee2cfebefcd9c4bfc30f6
VigneshSS11 <144017519+VigneshSS11@users.noreply.github.com>
2023-11-05 14:19:59 +0530

    Update no of days in month wrt years.c

--- no of days in month wrt years (error).c
+++ no of days in month wrt years (error).c
@@ -7,9 +7,9 @@ int main()
 	scanf("%d",&m);
 	printf("Enther the year:");
 	scanf("%d",&y);
-	if(m==1,3,5,7,8,10,12)
+	if(m==1||m==3||m==5||m==7||m==8||m==10||m==12)
 	printf("The no. of days is 31");
-	else if(m==4,6,9,11)
+	else if(m==4||m==6||m==9||m==11)
 	printf("The no of days is 30");
 	else
 	{
